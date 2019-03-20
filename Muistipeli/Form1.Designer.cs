b19982a7f0ad9547aee866e75daf0ceeae7f7922
Lahtinen Kalle <ao186696@gradia.fi>
2019-03-20 12:40:42 +0200

    Add project files.

---
+++ Muistipeli/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Muistipeli
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameControlGroupBox = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.gameGroupBox = new System.Windows.Forms.GroupBox();
+            this.CardHolder = new System.Windows.Forms.Label();
+            this.statsGroupBox = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameControlGroupBox.SuspendLayout();
+            this.gameGroupBox.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameControlGroupBox
+            // 
+            this.gameControlGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gameControlGroupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gameControlGroupBox.Controls.Add(this.textBox1);
+            this.gameControlGroupBox.Controls.Add(this.btnStart);
+            this.gameControlGroupBox.Location = new System.Drawing.Point(1, 23);
+            this.gameControlGroupBox.Name = "gameControlGroupBox";
+            this.gameControlGroupBox.Size = new System.Drawing.Size(1079, 217);
+            this.gameControlGroupBox.TabIndex = 0;
+            this.gameControlGroupBox.TabStop = false;
+            this.gameControlGroupBox.Text = "Controls";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(28, 39);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // gameGroupBox
+            // 
+            this.gameGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gameGroupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gameGroupBox.Controls.Add(this.CardHolder);
+            this.gameGroupBox.Location = new System.Drawing.Point(1, 108);
+            this.gameGroupBox.Name = "gameGroupBox";
+            this.gameGroupBox.Size = new System.Drawing.Size(1084, 814);
+            this.gameGroupBox.TabIndex = 1;
+            this.gameGroupBox.TabStop = false;
+            this.gameGroupBox.Text = "Game";
+            // 
+            // CardHolder
+            // 
+            this.CardHolder.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CardHolder.BackColor = System.Drawing.Color.Silver;
+            this.CardHolder.Location = new System.Drawing.Point(5, 16);
+            this.CardHolder.Name = "CardHolder";
+            this.CardHolder.Size = new System.Drawing.Size(1079, 765);
+            this.CardHolder.TabIndex = 0;
+            // 
+            // statsGroupBox
+            // 
+            this.statsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statsGroupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.statsGroupBox.BackColor = System.Drawing.SystemColors.Info;
+            this.statsGroupBox.Location = new System.Drawing.Point(1, 905);
+            this.statsGroupBox.Name = "statsGroupBox";
+            this.statsGroupBox.Size = new System.Drawing.Size(1085, 78);
+            this.statsGroupBox.TabIndex = 1;
+            this.statsGroupBox.TabStop = false;
+            this.statsGroupBox.Text = "Stats";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1088, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitGameToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            // 
+            // exitGameToolStripMenuItem
+            // 
+            this.exitGameToolStripMenuItem.BackColor = System.Drawing.Color.OrangeRed;
+            this.exitGameToolStripMenuItem.Name = "exitGameToolStripMenuItem";
+            this.exitGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitGameToolStripMenuItem.Text = "Exit Game";
+            this.exitGameToolStripMenuItem.Click += new System.EventHandler(this.exitGameToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 995);
+            this.Controls.Add(this.statsGroupBox);
+            this.Controls.Add(this.gameGroupBox);
+            this.Controls.Add(this.gameControlGroupBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(1100, 1038);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory Game";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.gameControlGroupBox.ResumeLayout(false);
+            this.gameControlGroupBox.PerformLayout();
+            this.gameGroupBox.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gameControlGroupBox;
+        private System.Windows.Forms.GroupBox gameGroupBox;
+        private System.Windows.Forms.GroupBox statsGroupBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitGameToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label CardHolder;
+    }
+}
+
---
+++ Muistipeli/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Muistipeli
+{
+    public partial class MainForm : Form
+    {
+        Point dot = new Point(0,0);
+       
+        PictureBox[] GameCard;
+        Random location = new Random();
+        List<Point> points = new List<Point>();
+        bool again = false;
+        PictureBox[] CoverCard = new PictureBox[16];
+        int i = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            //  dot = ;
+
+            for (int y = 0; y < 5; y++)
+            {
+                for (i = 0; i < 7; i++)
+                {
+                    CoverCard[i] = new PictureBox();
+                    CreateCoverCard(CoverCard[i]);
+                    dot.X = dot.X + 153;
+                }
+                dot.Y = dot.Y + 153;
+                dot.X = 0;
+            }
+            
+
+        }
+
+        private  void CreateCoverCard (PictureBox CoverCard)
+            {
+
+            
+            CoverCard.Size = new System.Drawing.Size(150,150);
+            CoverCard.Image = Properties.Resources.thinkingsmiley;
+            CoverCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            CoverCard.Parent = CardHolder;
+            CoverCard.Location = dot;
+            CoverCard.MouseClick += CardHolder_Click ;
+            
+            
+
+            }
+
+    private void exitGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void CardHolder_Click(object sender, EventArgs e)
+        {
+            PictureBox card = (PictureBox)sender;
+            card.Visible = false;
+        }
+    }
+}
